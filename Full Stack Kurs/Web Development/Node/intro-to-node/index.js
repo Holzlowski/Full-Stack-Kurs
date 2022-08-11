0be16d3bd6cba061b26f9994ebff9378fee1af75
Holzlowski <43970216+Holzlowski@users.noreply.github.com>
2022-08-11 18:07:34 +0200

    Geordnet

---
+++ Full Stack Kurs/Web Development/Node/intro-to-node/index.js
@@ -0,0 +1,10 @@
+const superheroes = require('superheroes');
+
+var mySuperHeroName = superheroes.random();
+
+const supervillains = require('supervillains');
+
+var mySuperVillainName = supervillains.random();
+
+console.log(mySuperHeroName);
+console.log(mySuperVillainName);
