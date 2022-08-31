71ae048f7c878dc850c640bc852cc708854b6284
Holzlowski <43970216+Holzlowski@users.noreply.github.com>
2022-08-31 13:42:31 +0200

    Email Newsletter Fail

--- Full Stack Kurs/Web Development/Node/Calculator/calculator.js
+++ Full Stack Kurs/Web Development/Node/Calculator/calculator.js
@@ -18,6 +18,21 @@ var result = num1 + num2;
   res.send("The result of the Calculation is " + result);
 });
 
+app.get("/bmiCalculator.html", function(req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmiCalculator.html", function(req, res) {
+
+var weight = parseFloat(req.body.weight);
+var height = parseFloat(req.body.height);
+
+var bmi = weight / (height * height);
+
+res.send("Your BMI is: " + bmi);
+
+})
+
 app.listen(3000, function() {
 console.log("Server started on port 3000.");
 });
