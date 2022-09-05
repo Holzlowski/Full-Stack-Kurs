ba96c5b15c998e94c6cdfb0071958d68efcdce07
David Holz <david.holz@fokus.fraunhofer.de>
2022-09-05 09:12:50 +0200

    haraku server

--- Full Stack Kurs/Web Development/Newsletter-Signup/app.js
+++ Full Stack Kurs/Web Development/Newsletter-Signup/app.js
@@ -64,7 +64,7 @@ app.post("/failure", function(req, res) {
   res.redirect("/");
 })
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Verbindung läuft JUUUUNGE! Auf Port 3000");
 })
 
