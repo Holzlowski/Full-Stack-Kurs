43c3998b2d370a63595a0ef541ad8188cfe1cf1e
Holzlowski <43970216+Holzlowski@users.noreply.github.com>
2025-08-17 15:18:25 +0200

    delete old backend projects

--- Full Stack Kurs/Backend/Backend/2.3 NPM/index.js
+++
@@ -1,8 +0,0 @@
-import generatedName from "sillyname"
-import superheroes from "superheroes"
-//var generatedName = require('sillyname');
-var sillyname = generatedName();
-var superHeroName = superheroes.random();
-
-console.log(`I bims, der ${superHeroName}.`);
-console.log(`Meine wahre Identität ist ${sillyname}.`);
